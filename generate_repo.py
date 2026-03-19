e9db0203ed1f3aa05ac7e9fe042309f9480ebdd9
Mikykino <mikysedliste@seznam.cz>
2026-03-19 21:29:15 +0100

    Update generate_repo.py

--- generate_repo.py
+++ generate_repo.py
@@ -1,4 +1,4 @@
-import os, zipfile, hashlib, xml.etree.ElementTree as ET
+import os, zipfile, hashlib, shutil, xml.etree.ElementTree as ET
 
 ADDONS_DIR = "addons"
 ZIPS_DIR = "zips"
@@ -17,11 +17,11 @@ for addon_id in os.listdir(ADDONS_DIR):
     version = root.get("version")
     addons_xml_root.append(root)
 
-    # ZIP do podsložky addon_id
+    zip_name = f"{addon_id}-{version}.zip"
+    
+    # ZIP do podsložky (pro instalaci z repozitáře)
     addon_zip_dir = os.path.join(ZIPS_DIR, addon_id)
     os.makedirs(addon_zip_dir, exist_ok=True)
-
-    zip_name = f"{addon_id}-{version}.zip"
     zip_path = os.path.join(addon_zip_dir, zip_name)
     with zipfile.ZipFile(zip_path, "w", zipfile.ZIP_DEFLATED) as zf:
         for dirpath, _, files in os.walk(addon_path):
@@ -29,6 +29,9 @@ for addon_id in os.listdir(ADDONS_DIR):
                 filepath = os.path.join(dirpath, f)
                 arcname = os.path.relpath(filepath, ADDONS_DIR)
                 zf.write(filepath, arcname)
+    
+    # Kopie ZIP přímo do zips/ (pro instalaci ze souboru ZIP)
+    shutil.copy(zip_path, os.path.join(ZIPS_DIR, zip_name))
     print(f"Zipped: {zip_name}")
 
 addons_xml_str = ET.tostring(addons_xml_root, encoding="unicode")
@@ -39,14 +42,14 @@ md5 = hashlib.md5(addons_xml_str.encode()).hexdigest()
 with open("zips/addons.xml.md5", "w") as f:
     f.write(md5)
 
-# Generate index.html for zips directory
+# Generate index.html
+zip_files = [f for f in os.listdir(ZIPS_DIR) if f.endswith('.zip')]
 with open("zips/index.html", "w") as f:
     f.write("<html><body>\n")
     f.write('<a href="addons.xml">addons.xml</a><br>\n')
     f.write('<a href="addons.xml.md5">addons.xml.md5</a><br>\n')
-    for d in sorted(os.listdir(ZIPS_DIR)):
-        if os.path.isdir(os.path.join(ZIPS_DIR, d)):
-            f.write(f'<a href="{d}/">{d}/</a><br>\n')
+    for z in sorted(zip_files):
+        f.write(f'<a href="{z}">{z}</a><br>\n')
     f.write("</body></html>\n")
 
-print("Done: addons.xml + addons.xml.md5 generated")
+print("Don
